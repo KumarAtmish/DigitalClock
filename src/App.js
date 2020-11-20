f33860980a6b70a9355ab382f8cf16b720ec6f6c
KumarAtmish <73941481+KumarAtmish@users.noreply.github.com>
2020-11-20 18:59:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  let time = new Date().toLocaleTimeString();
+  const [ctime, setCtime] = useState(time);
+
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+
+  setInterval(UpdateTime, 1000);
+
+  return (
+    <div className="App">
+      <h1>{time}</h1>
+    </div>
+  );
+}
